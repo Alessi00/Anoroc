9756fca486d4c1edf66e5cf3d80861e48ebd5272
Andrew96-eng <u15191223@tuks.co.za>
2020-09-09 14:36:01 +0200

    Added structure for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header/header.component';
+import { MapComponent } from './Map/map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
