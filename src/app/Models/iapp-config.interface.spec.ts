384784dcc8fd31d0e34243ffb7cb539f85cf9d6b
Andrew <61750301+Andrew96-eng@users.noreply.github.com>
2020-09-14 19:24:06 +0200

    Update iapp-config.interface.spec.ts

--- src/app/Models/iapp-config.interface.spec.ts
+++ src/app/Models/iapp-config.interface.spec.ts
@@ -1,7 +1,3 @@
 import { IAppConfig } from './iapp-config.interface';
 
-describe('IAppConfig', () => {
-  it('should create an instance', () => {
-    expect(new IAppConfig()).toBeTruthy();
-  });
-});
+
