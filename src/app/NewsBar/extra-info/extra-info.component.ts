0df9c1bc8e16323b2692cf2ef4eceefa2e852340
Andrew96-eng <u15191223@tuks.co.za>
2020-10-11 07:55:44 +0200

    Added styling for different sizes

--- src/app/NewsBar/extra-info/extra-info.component.ts
+++ src/app/NewsBar/extra-info/extra-info.component.ts
@@ -13,8 +13,40 @@ export class ExtraInfoComponent implements OnInit {
   {
     this.router = r;
   }
-
+  innerWidth: any;
   ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerWidth);
+    if(this.innerWidth <= 1280)
+    {
+      var clickheading = document.getElementById('clickheading');
+      var gdprpic = document.getElementById('gdprpic');
+      var downloadApp = document.getElementById('downloadApp');
+      gdprpic.style.width = "10em";
+      gdprpic.style.height = "8em";
+      clickheading.style.width = "25em";
+      clickheading.style.height = "8em";
+      downloadApp.style.width = "20em";
+    }
+    else if(this.innerWidth <= 1550)
+    {
+      var canvasobj = document.getElementsByTagName('canvas')[0];
+      var datasource = document.getElementById('datasourcebtn');
+      datasource.style.width = "600px";
+      canvasobj.width  = 600;
+      canvasobj.height = 280;
+      var gdprpic = document.getElementById('gdprpic');
+      gdprpic.style.width = "10em";
+      gdprpic.style.height = "8em";
+    }
+    else
+    {
+      var canvasobj = document.getElementsByTagName('canvas')[0];
+      canvasobj.width  = 750;
+      canvasobj.height = 350;
+      var datasource = document.getElementById('datasourcebtn');
+      datasource.style.width = "755px";
+    }
   }
 
   routePrivacy()
